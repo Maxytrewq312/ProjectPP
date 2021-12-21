82078d69aca986c9990c86dafa978c1d4edcd311
Maxytrewq312 <63639574+Maxytrewq312@users.noreply.github.com>
2021-12-21 15:10:02 +0300

    1
    2

---
+++ SQLQuery2.sql
@@ -0,0 +1,4 @@
+SELECT Price = SUM(Price*Quantity) from Products 
+JOIN Orders_Products ON Orders_Products.Id_prod = Products.Id_prod
+JOIN Orders ON Orders.Id_order = Orders_Products.Id_order
+JOIN Users ON Users.Id_us = Orders.Id_us
